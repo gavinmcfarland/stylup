b502bebc469d345f7951db3f2cc694db35c6e88e
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-07-14 02:24:26 +0100

    added condition to check for class name

--- src/flatten-classes.js
+++ src/flatten-classes.js
@@ -5,47 +5,49 @@ import re from './util/generate-regex.js'
 
 export default function (node) {
 
-	let classNames = node.attrs.get('class').split(' ')
-	let newClassNames = []
+	if (node.attrs.get('class')) {
+		let classNames = node.attrs.get('class').split(' ')
+		let newClassNames = []
 
-	_.each(classNames, function (className) {
+		_.each(classNames, function (className) {
 
-		if (className.match(re.decl)) {
-			className.replace(re.decl, function(decl, name, args) {
+			if (className.match(re.decl)) {
+				className.replace(re.decl, function(decl, name, args) {
 
-				if (name === 'm' || 'p') {
-					// Currently only anticipates things like margin, padding, etc
+					if (name === 'm' || 'p') {
+						// Currently only anticipates things like margin, padding, etc
 
-					let values = []
-					let sides = ['t', 'r', 'b', 'l']
+						let values = []
+						let sides = ['t', 'r', 'b', 'l']
 
-					args.replace(re.arg, function(arg) {
-						values.push(arg);
-					})
+						args.replace(re.arg, function(arg) {
+							values.push(arg);
+						})
 
-					if (values.length === 1) {
-						values.push(values[0]);
+						if (values.length === 1) {
+							values.push(values[0]);
+						}
+						if (values.length === 2) {
+							values.push(values[0]);
+						}
+						if (values.length === 3) {
+							values.push(values[1]);
+						}
+
+						_.each(sides, function(side, index) {
+							newClassNames.push(`${name}${side}-${values[index]}`)
+						})
 					}
-					if (values.length === 2) {
-						values.push(values[0]);
-					}
-					if (values.length === 3) {
-						values.push(values[1]);
-					}
-
-					_.each(sides, function(side, index) {
-						newClassNames.push(`${name}${side}-${values[index]}`)
-					})
-				}
 
 
-			})
-		}
-		else {
-			newClassNames.push(className)
-		}
+				})
+			}
+			else {
+				newClassNames.push(className)
+			}
 
-	})
+		})
 
-	node.attrs.add({'class': newClassNames.join(" ")})
+		node.attrs.add({'class': newClassNames.join(" ")})
+	}
 }
